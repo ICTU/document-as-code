20f40eddbb0c518fe1de42ef88c90c0579b8331e
Eric Nieuwland <eric.nieuwland@gmail.com>
2016-10-03 16:06:40 +0200

    Add builder pattern

--- python/reference/url/confluence_url.py
+++ python/reference/url/confluence_url.py
@@ -25,16 +25,43 @@ class ConfluenceUrl( object ):
             https://confluence.atlassian.com/display/DOC/Working+with+Links#WorkingwithLinks-Insertalink
     """
 
+    _base_url    = None
+    _page_title  = None
+    _anchor_name = None
 
-    def __init__( self, base_url, page_title, anchor_name=None ):
+
+    def __init__( self, base_url, page_title=None, anchor_name=None ):
         """
         @param base_url: base of all URLs, including the spacekey
         @param page_title: title of the page as displayed
         @param anchor_name: name of the anchor, also works for headings
         """
-        self.base_url    = base_url
-        self.page_title  = page_title
-        self.anchor_name = anchor_name
+        self._base_url    = base_url
+        self._page_title  = page_title
+        self._anchor_name = anchor_name
+
+
+    def __str__( self ):
+        return "{cls.__name__}( {b!r}, {p!r}, {a!r} )".format( cls=self.__class__, b=self._base_url, p=self._page_title, a=self._anchor_name )
+
+    __repr__ = __str__
+
+
+    def page_title( self, page_title, anchor_name=None ):
+        """
+        build on top of an existing base URL
+        @param page_title: title of the page as displayed
+        @param anchor_name: name of the anchor, also works for headings
+        """
+        return self.__class__( self._base_url, page_title, anchor_name if anchor_name else self._anchor_name )
+
+
+    def anchor_name( self, anchor_name ):
+        """
+        build on top of an existing base URL and page title
+        @param anchor_name: name of the anchor, also works for headings
+        """
+        return self.__class__( self._base_url, self._page_title, anchor_name )
 
 
     @property
@@ -42,14 +69,17 @@ class ConfluenceUrl( object ):
         """
         compose the actual URL from its parts
         """
-        page_in_url = quote_plus( self.page_title )
-        url = '/'.join( ( self.base_url, page_in_url ) )
+        url = self._base_url
 
-        if self.anchor_name:
-            page_in_anchor = self.page_title.replace( ' ', '' )
-            anchor_name_in_anchor = self.anchor_name.replace( ' ', '' )
-            anchor = '-'.join( ( page_in_anchor, anchor_name_in_anchor ) )
-            url = '#'.join( ( url, anchor ) )
+        if self._page_title:
+            page_in_url = quote_plus( self._page_title )
+            url = '/'.join( ( url, page_in_url ) )
+
+            if self._anchor_name:
+                page_in_anchor = self._page_title.replace( ' ', '' )
+                anchor_name_in_anchor = self._anchor_name.replace( ' ', '' )
+                anchor = '-'.join( ( page_in_anchor, anchor_name_in_anchor ) )
+                url = '#'.join( ( url, anchor ) )
 
         return url
 
