56c204a9c68aa4eca518db442abe4135b745c919
ernie <eric.nieuwland@ictu.nl>
2019-11-20 15:46:40 +0100

    REFACTOR define BIR model as derived from BIO model

--- python/domain/bir/model/norm.py
+++ python/domain/bir/model/norm.py
@@ -2,22 +2,4 @@
     norm - defines and describes a norm
 """
 
-from __future__ import absolute_import
-from __future__ import print_function
-
-
-from domain.document.model import recursive_fragment
-
-
-class Norm(recursive_fragment.RecursiveFragment):
-
-    bbn = None
-
-    def __init__(self, identifier, title, bbn=None, lead=None, text=None, fragments=[]):
-        """
-        create a norm
-        BIR 2017 introduced a security level (basisbeschermingsniveau, BBN)
-        """
-        super(Norm, self).__init__(identifier, title, lead=lead, text=text, fragments=fragments)
-
-        self.bbn = bbn
+from domain.bio.model.norm import Norm
--- python/domain/bir/model/verifier.py
+++ python/domain/bir/model/verifier.py
@@ -2,22 +2,4 @@
     verifier - defines and describes a verifier with a norm
 """
 
-from __future__ import absolute_import
-from __future__ import print_function
-
-
-from domain.document.model import recursive_fragment
-
-
-class Verifier(recursive_fragment.RecursiveFragment):
-
-    bbn = None
-
-    def __init__(self, identifier, title, bbn=None, lead=None, text=None, fragments=[]):
-        """
-        create a verifier
-        BIR 2017 introduced a security level (basisbeschermingsniveau, BBN)
-        """
-        super(Verifier, self).__init__(identifier, title, lead=lead, text=text, fragments=fragments)
-
-        self.bbn = bbn
+from domain.bio.model.verifier import Verifier
